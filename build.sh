d2bc5df1131565d83443183641eb358a40773955
ZxroxXm <shinema3743@gmail.com>
2025-07-12 14:33:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -166,9 +166,9 @@ lunch lineage_a04e-ap2a-userdebug
 mka bacon
 
 rm -rf hardware_legacy
-/opt/crave/resync.sh
 rm -rf vendor/samsung/a04e
 git clone https://github.com/ZxroxXm/vendor_samsung_a04e -b lineage-21 vendor/samsung/a04e
+/opt/crave/resync.sh
 source build/envsetup.sh
 lunch lineage_a04e-ap2a-userdebug
 mka bacon
